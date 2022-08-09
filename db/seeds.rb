308f01b5a4946fc0ff64b674e3f64a3070037e2e
Nic Rothquel <nrothquel@gmail.com>
2022-08-09 15:23:22 -0400

    index and add functions completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+Article.destroy_all
+
+10.times {
+  title = Faker::Superhero.name
+  content = Faker::Lorem.paragraph
+  article = Article.new(title: title, content: content)
+  article.save!}
