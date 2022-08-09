0df35b5e7399d423dcc050c4ab06c7530d4fdcbb
Nic Rothquel <nrothquel@gmail.com>
2022-08-09 15:50:06 -0400

    completed exercise

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,12 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to article_path, status: :see_other
+  end
+
   private
 
   def article_params
